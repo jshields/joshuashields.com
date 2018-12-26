9b2610747884352276615f00c0b8d7002b1d2fd2
jshields <shields.h.josh@gmail.com>
2018-12-25 23:58:54 -0600

    adjust comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 //require.extensions['.scss'] = () => { return; }; require.extensions['.css'] = () => { return; };
-require.extensions['.png'] = () => { return; }; require.extensions['.jpg'] = () => { return; };
+// doesn't seem to do anything
+//require.extensions['.png'] = () => { return; }; require.extensions['.jpg'] = () => { return; };
 
 module.exports = {
 
@@ -15,8 +16,18 @@ module.exports = {
     },*/
     //entry: path.resolve(__dirname, 'src/js/main.js'),  // not needed, not using modules for JS
 
-    // TODO fix url(...) breaking main.scss compilation
-    // https://github.com/webpack-contrib/sass-loader#problems-with-url
+    /*
+    TODO fix url(...) breaking main.scss compilation
+    Why does Webpack / some loader think it needs to to something with image URLs?
+    How does it even know what they are?
+    https://developer.mozilla.org/en-US/docs/Web/CSS/url
+
+    https://github.com/postcss/postcss-loader/issues/160#issuecomment-271082726
+
+    https://github.com/webpack-contrib/sass-loader#problems-with-url
+    https://github.com/webpack-contrib/url-loader
+    https://github.com/webpack-contrib/file-loader
+    */
     entry: path.resolve(__dirname, 'src/scss/main.scss'),
 
     // https://stackoverflow.com/q/29760894/3538313
@@ -24,14 +35,18 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [{
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,  // put styles into an actual file
-                "css-loader", // translates CSS into CommonJS
-                "sass-loader",  // compiles Sass to CSS, using Node Sass by default
-            ]
-        }]
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,  // put styles into an actual file
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader",  // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            // TODO use url-loader or file-loader to handle image files that scss -> css pipeline is finding via `url(...)` values
+            //{ test: /\.(png|woff|woff2|eot|ttf|svg)$/, use: { loader: 'url-loader?limit=100000' } }
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
